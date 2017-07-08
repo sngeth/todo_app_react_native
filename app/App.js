e21d92659e8d253db5f4443ad893a4135a0c32de
Sid Ngeth <sngeth@gmail.com>
2017-07-08 17:01:15 -0400

    Add Input and Button components

--- app/App.js
+++ app/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import { View, ScrollView, StyleSheet } from 'react-native'
 import Heading from './Heading'
+import Input from './Input/'
+import Button from './Button'
+
+let todoIndex = 0
 
 class App extends Component {
   constructor() {
@@ -10,15 +14,44 @@ class App extends Component {
       todos: [],
       type: 'All'
     }
+    this.submitTodo = this.submitTodo.bind(this)
+  }
+
+  inputChange(inputValue) {
+    console.log(' Input Value: ' , inputValue)
+    this.setState({ inputValue })
+  }
+
+  submitTodo () {
+    if (this.state.inputValue.match(/^\s*$/)) {
+      return
+    }
+
+    let todo = {
+      title: this.state.inputValue,
+      todoIndex: todoIndex,
+      complete: false
+    }
+
+    todoIndex++
+    this.state.todos.push(todo)
+    this.setState({ todos: this.state.todos, inputValue: '' }, () => {
+      console.log('State: ', this.state)
+    })
   }
 
   render() {
+    const { inputValue } = this.state
     return (
       <View style={styles.container}>
         <ScrollView
           keyboardShouldPersistTaps="always"
           style={styles.container.content}>
           <Heading />
+          <Input
+            inputValue={inputValue}
+            inputChange={(text) => this.inputChange(text)} />
+          <Button submitTodo={this.submitTodo} />
         </ScrollView>
       </View>
     )
