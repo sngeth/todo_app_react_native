ff26f002606a37c3b4ed87e9a372b970d822bdaf
Sid Ngeth <sngeth@gmail.com>
2017-07-08 15:43:42 -0400

    Add a Heading component

---
+++ app/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { View, ScrollView, StyleSheet } from 'react-native'
+import Heading from './Heading'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      inputValue: '',
+      todos: [],
+      type: 'All'
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView
+          keyboardShouldPersistTaps="always"
+          style={styles.container.content}>
+          <Heading />
+        </ScrollView>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5'
+  },
+  content: {
+    flex: 1,
+    paddingTop: 60
+  }
+})
+
+export default App
